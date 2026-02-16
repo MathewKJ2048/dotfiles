c550ea70691b2d3736738a3a658d4c49d1de2f31
Mathew Kuthur James <mathewkj2048@gmail.com>
2026-02-16 01:14:16 -0500

    added a flake

--- nix-home-manager/flake.nix
+++ nix-home-manager/flake.nix
@@ -1,15 +1,27 @@
 {
-  description = "A very basic flake";
+  description = "Home-manager flake";
+  # this flake describes both the installation of home-manager, and the source that home-manager follows once installed
 
   inputs = {
+    # source of truth
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+
+    # Home Manager - follows our nixpkgs for consistency
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
   };
 
-  outputs = { self, nixpkgs }: {
+  outputs = { self, nixpkgs, home-manager, ... }: 
 
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+    let 
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in 
+    {
+    packages.${system}.default = home-manager.packages.${system}.default;
+    };
 
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
-  };
 }
