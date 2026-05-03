7bcb2bfd995c1fbfb4ebd619c197551c9de40545
Mathew Kuthur James <mathewkj2048@gmail.com>
2026-05-03 03:04:09 -0400

    guake

---
+++ terminals/guake.nix
@@ -0,0 +1,9 @@
+{ config, pkgs, userConf, ... }: {
+
+  # home.packages = with pkgs; [
+  # ] ++ lib.optional userConf.CinnamonX11 tdrop;
+
+  # guake needed only if cinnamon X11 and isNixOS, but config needed always
+  
+  
+}
