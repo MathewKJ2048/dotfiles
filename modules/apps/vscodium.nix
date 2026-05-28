d661832145bcb9a4a42bb9da5de4b7f5fd365395
Mathew Kuthur James <mathewkj2048@gmail.com>
2026-05-28 17:33:35 -0400

    codium

--- modules/apps/vscodium.nix
+++ modules/apps/vscodium.nix
@@ -8,7 +8,7 @@
 
   home.file = {
     "${config.xdg.configHome}/VSCodium/User/settings.json" = {
-      source = config.lib.file.mkOutOfStoreSymlink "${userConf.thisDirectory}/build/settings.json";
+      source = config.lib.file.mkOutOfStoreSymlink "${userConf.thisDirectory}/config/vscodium-settings.json";
       force = true;
     };
   };
