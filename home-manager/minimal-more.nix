7d6a06bb05e467f8aabd1b24c17da72c81f0780d
Mathew Kuthur James <mathewkj2048@gmail.com>
2026-05-29 11:27:12 -0400

    error

---
+++ home-manager/minimal-more.nix
@@ -0,0 +1,15 @@
+{ pkgs, config, userConf, systemConf, ... }:
+
+{
+  imports = [
+    ./minimal-headless.nix
+
+    ../modules/apps/vscodium.nix # text editor
+    ../modules/apps/browsers/firefox.nix  # browser
+    ../modules/apps/terminals/ghostty.nix  # terminal
+
+    ../modules/networking/networking.nix
+    ../modules/theme/theme.nix
+    ../modules/tools/tools.nix
+  ];
+}
--- home-manager/minimal.nix
+++ home-manager/minimal.nix
@@ -1,15 +1,11 @@
+{ pkgs, userConf, isNixOS, ... }:
+
 { pkgs, config, userConf, systemConf, ... }:
 
 {
   imports = [
-    ./minimal-headless.nix
-
-    ../modules/apps/vscodium.nix # text editor
-    ../modules/apps/browsers/firefox.nix  # browser
-    ../modules/apps/terminals/ghostty.nix  # terminal
-
-    ../modules/networking/networking.nix
-    ../modules/theme/theme.nix
-    ../modules/tools/tools.nix
+    ./common.nix
+    ../modules/development/git.nix
+    ../modules/shell/shell.nix
   ];
 }
