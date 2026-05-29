c14ae93e95e21ad958e564bd8342275592cfc8fd
Mathew Kuthur James <mathewkj2048@gmail.com>
2026-05-29 00:01:24 -0400

    bootstrap

--- TODO.md
+++ TODO.md
@@ -43,3 +43,33 @@
 - imagemagick for wallpapers
 - icon recoloring
 - auto-install personal scripts
+
+
+```
+	userConf = rec {
+        name = "mathew";
+        fullName = "Mathew Kuthur James";
+        email = "mathewkj2048@gmail.com"; 
+
+        username = name;
+        homeDirectory = "/home/${username}";
+        downloadsDirectory = "${homeDirectory}/Downloads"; 
+        desktopDirectory = "${homeDirectory}/Desktop";
+        thisDirectory = "${homeDirectory}/Projects/dotfiles"; # path to where this repo itself is stored when cloned
+        screenshotsDirectory = desktopDirectory; # location to which screenshots are saved
+        videoDownloadDirectory = desktopDirectory; # locations to which downloaded videos are saved
+        
+        gitArgs  = {
+          inherit email; # email for git, used for signing commits
+          name = fullName; # name for git, used for signing commits
+        };
+      };
+
+
+	systemConf = {
+		isNixOS = true;
+		KdeWayland = false;
+		CinnamonX11 = false;
+	};
+
+```
--- flake/flake.nix
+++ flake/flake.nix
@@ -8,103 +8,144 @@
     };
   };
 
-  outputs =
-    { nixpkgs, home-manager, ... }:
-    let
-      userConf = rec {
-        name = "mathew";
-        fullName = "Mathew Kuthur James";
-        email = "mathewkj2048@gmail.com"; 
+  outputs = { nixpkgs, home-manager, ... }:
+  let
+    userConf = rec {
+      name = "mathew";
+      fullName = "Mathew Kuthur James";
+      email = "mathewkj2048@gmail.com"; 
 
-        username = name;
-        homeDirectory = "/home/${username}";
-        downloadsDirectory = "${homeDirectory}/Downloads"; 
-        desktopDirectory = "${homeDirectory}/Desktop";
-        thisDirectory = "${homeDirectory}/Projects/dotfiles"; # path to where this repo itself is stored when cloned
-        screenshotsDirectory = desktopDirectory; # location to which screenshots are saved
-        videoDownloadDirectory = desktopDirectory; # locations to which downloaded videos are saved
-        
-        gitArgs  = {
-          inherit email; # email for git, used for signing commits
-          name = fullName; # name for git, used for signing commits
-        };
+      username = name;
+      homeDirectory = "/home/${username}";
+      downloadsDirectory = "${homeDirectory}/Downloads"; 
+      desktopDirectory = "${homeDirectory}/Desktop";
+      thisDirectory = "${homeDirectory}/Projects/dotfiles"; # path to where this repo itself is stored when cloned
+      screenshotsDirectory = desktopDirectory; # location to which screenshots are saved
+      videoDownloadDirectory = desktopDirectory; # locations to which downloaded videos are saved
+      
+      gitArgs  = {
+        inherit email; # email for git, used for signing commits
+        name = fullName; # name for git, used for signing commits
       };
-       
+    };
+  in
+  {
+    homeConfigurations.default = 
+    let
+      systemConf = {
+        isNixOS = false;
+        KdeWayland = false;
+        CinnamonX11 = false;
+      };
     in
-    {
-      nixosConfigurations."bootstrap" = 
-      let 
-        systemConf = {
-          isNixOS = true;
-          KdeWayland = false;
-          CinnamonX11 = false;
-        };
-      in nixpkgs.lib.nixosSystem {
-          modules = [
-            ../configuration.nix
-            home-manager.nixosModules.home-manager
-            {
-              home-manager.useGlobalPkgs = true; # use the same nixpkgs as the nixos system
-              home-manager.useUserPackages = true; # prevent creation of a separate .nix-profile 
-              home-manager.users.${userConf.username} = import ../home-manager/common.nix;
-              home-manager.extraSpecialArgs = {
-                inherit userConf;
-                inherit systemConf;
-              };
-            }
-          ];
-          specialArgs = {
-            inherit userConf;
-            inherit systemConf;
-          };
+    home-manager.lib.homeManagerConfiguration {
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      modules = [ 
+        ../home-manager/default.nix
+      ];
+      extraSpecialArgs = {
+        inherit userConf;
+        inherit systemConf;
+      };
+    };
+
+    nixosConfigurations."bootstrap" = 
+    let 
+      systemConf = {
+        isNixOS = true;
+        KdeWayland = false;
+        CinnamonX11 = false;
+        hostName = "bootstrap";
+      };
+      specialArgs = {
+        inherit userConf;
+        inherit systemConf;
+      };
+    in nixpkgs.lib.nixosSystem {
+        modules = [
+          ../configuration
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true; # use the same nixpkgs as the nixos system
+            home-manager.useUserPackages = true; # prevent creation of a separate .nix-profile 
+            home-manager.users.${userConf.username} = import ../home-manager/minimal.nix;
+            home-manager.extraSpecialArgs = specialArgs;
+          }
+        ];
+        specialArgs = specialArgs;
       };
 
     };
 
-    homeConfigurations."default" = 
-      let
-        systemConf = {
-          isNixOS = false;
-          KdeWayland = false;
-          CinnamonX11 = true;
-        };
-      in home-manager.lib.homeManagerConfiguration {
-        pkgs = nixpkgs.legacyPackages.x86_64-linux;
-        modules = [ 
-          ../home-manager/default.nix
-        ];
-        extraSpecialArgs = {
-          inherit userConf;
-          inherit systemConf;
-        };
-      };
-
-      nixosConfigurations."G24L061" = 
-      let 
-        systemConf = {
-          isNixOS = true;
-          KdeWayland = true;
-          CinnamonX11 = true;
-        };
-        specialArgs = {
-          inherit userConf;
-          inherit systemConf;
-        };
-      in nixpkgs.lib.nixosSystem {
-          modules = [
-            ../nixos/configurations/laptop.nix
-            ../hardware-configurations/ThinkPad-P14s-Gen-5.nix
-            home-manager.nixosModules.home-manager
-            {
-              home-manager.useGlobalPkgs = true; # use the same nixpkgs as the nixos system
-              home-manager.useUserPackages = true; # prevent creation of a separate .nix-profile 
-              home-manager.users.${userConf.username} = import ../home-manager/default.nix;
-              home-manager.extraSpecialArgs = specialArgs;
-            }
-          ];
-          specialArgs = specialArgs;
-      };
 }
+    #   nixosConfigurations."bootstrap" = nixpkgs.lib.nixosSystem {
+    #       modules = [
+    #         ../configuration.nix
+    #         home-manager.nixosModules.home-manager
+    #         {
+    #           home-manager.useGlobalPkgs = true; # use the same nixpkgs as the nixos system
+    #           home-manager.useUserPackages = true; # prevent creation of a separate .nix-profile 
+    #           home-manager.users.${userConf.username} = import ../home-manager/common.nix;
+    #           # home-manager.extraSpecialArgs = {
+    #           #   inherit userConf;
+    #           #   inherit systemConf;
+    #           # };
+    #         }
+    #       ];
+    #       # specialArgs = {
+    #       #   inherit userConf;
+    #       #   inherit systemConf;
+    #       # };
+    #   };
+
+    # };
+
+    
+
+      # nixosConfigurations."G24L061" = 
+      # let 
+      #   userConf = rec {
+      #   name = "mathew";
+      #   fullName = "Mathew Kuthur James";
+      #   email = "mathewkj2048@gmail.com"; 
+
+      #   username = name;
+      #   homeDirectory = "/home/${username}";
+      #   downloadsDirectory = "${homeDirectory}/Downloads"; 
+      #   desktopDirectory = "${homeDirectory}/Desktop";
+      #   thisDirectory = "${homeDirectory}/Projects/dotfiles"; # path to where this repo itself is stored when cloned
+      #   screenshotsDirectory = desktopDirectory; # location to which screenshots are saved
+      #   videoDownloadDirectory = desktopDirectory; # locations to which downloaded videos are saved
+        
+      #   gitArgs  = {
+      #     inherit email; # email for git, used for signing commits
+      #     name = fullName; # name for git, used for signing commits
+      #   };
+      # };
+      #   systemConf = {
+      #     isNixOS = true;
+      #     KdeWayland = true;
+      #     CinnamonX11 = true;
+      #   };
+      #   specialArgs = {
+      #     inherit userConf;
+      #     inherit systemConf;
+      #   };
+      # in nixpkgs.lib.nixosSystem {
+      #     modules = [
+      #       ../nixos/configurations/laptop.nix
+      #       ../hardware-configurations/ThinkPad-P14s-Gen-5.nix
+      #       home-manager.nixosModules.home-manager
+      #       {
+      #         home-manager.useGlobalPkgs = true; # use the same nixpkgs as the nixos system
+      #         home-manager.useUserPackages = true; # prevent creation of a separate .nix-profile 
+      #         home-manager.users.${userConf.username} = import ../home-manager/default.nix;
+      #         home-manager.extraSpecialArgs = specialArgs;
+      #       }
+      #     ];
+      #     specialArgs = specialArgs;
+      # };
+# }
 
 /*
 Template: NixOS configuration
