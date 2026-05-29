67732f742ff860777a21565c90b70b5f908114de
Mathew Kuthur James <mathewkj2048@gmail.com>
2026-05-29 11:39:01 -0400

    stuff

---
+++ home-manager/minimal-headless.nix
@@ -0,0 +1,9 @@
+{ pkgs, config, userConf, systemConf, ... }:
+
+{
+  imports = [
+    ./common.nix
+    ../modules/development/git.nix
+    ../modules/shell/shell.nix
+  ];
+}
--- home-manager/minimal.nix
+++ home-manager/minimal.nix
@@ -2,8 +2,14 @@
 
 {
   imports = [
-    ./common.nix
-    ../modules/development/git.nix
-    ../modules/shell/shell.nix
+    ./minimal-headless.nix
+
+    ../modules/apps/vscodium.nix # text editor
+    ../modules/apps/browsers/firefox.nix  # browser
+    ../modules/apps/terminals/ghostty.nix  # terminal
+
+    ../modules/networking/networking.nix
+    ../modules/theme/theme.nix
+    ../modules/tools/tools.nix
   ];
 }
