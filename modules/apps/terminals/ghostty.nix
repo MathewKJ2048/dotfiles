0f5be3f8e26de868f7277efcb1aa0feb07b9c926
Mathew Kuthur James <mathewkj2048@gmail.com>
2026-05-29 23:16:16 -0400

    ghostty update

--- modules/apps/terminals/ghostty.nix
+++ modules/apps/terminals/ghostty.nix
@@ -3,8 +3,8 @@
   programs.ghostty.enable = systemConf.isNixOS;
   
   home.file = {
-    "${config.xdg.configHome}/ghostty/config" = {
-      source = config.lib.file.mkOutOfStoreSymlink "${userConf.thisDirectory}/config/ghostty-config";
+    "${config.xdg.configHome}/ghostty/config.ghostty" = {
+      source = config.lib.file.mkOutOfStoreSymlink "${userConf.thisDirectory}/config/config.ghostty";
       force = true;
     };
   };
