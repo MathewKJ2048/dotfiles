2e10b4435752b05045d1f7d4e911874c47c73a08
Mathew Kuthur James <mathewkj2048@gmail.com>
2026-06-06 18:51:09 -0400

    stuff

--- hosts/sphalerite/configuration.nix
+++ hosts/sphalerite/configuration.nix
@@ -50,7 +50,7 @@
   services.xserver.enable = true;
 
   # Enable the Cinnamon Desktop Environment.
-  services.xserver.displayManager.lightdm.enable = true;
+  services.displayManager.ly.enable = true;
   services.xserver.desktopManager.cinnamon.enable = true;
 
   # Configure keymap in X11
