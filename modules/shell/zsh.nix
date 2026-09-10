2609f1d783937b18a7381fbdce62c7300cf1dfb2
Mathew Kuthur James <mathewkj2048@gmail.com>
2026-09-10 02:20:14 -0400

    testing with new thing

--- modules/shell/zsh.nix
+++ modules/shell/zsh.nix
@@ -96,6 +96,16 @@
           git -C "$CWD" pull
           git -C "$CWD" push
       }
+      ju()
+      {
+        if [ -z "$1" ]; then
+              echo "Error: Please provide a commit message as the first argument."
+              exit 1
+        fi
+        jj describe -m "$*"
+        jj b a
+        jj git push
+      }
 
       try()
       {
